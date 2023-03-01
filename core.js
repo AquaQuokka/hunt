ebd67cee9061b6a310d27d9664c275fd3b538b84
AquaQuokka <101112829+AquaQuokka@users.noreply.github.com>
2023-03-01 20:24:20 +0800

    misc(files): move script to separate file

---
+++ core.js
@@ -0,0 +1,67 @@
+    
+        // Get a random number from 0 to "size".
+        var get_random_number = function(size) {
+            return Math.floor(Math.random() * size);
+        };
+
+        // Calculate the difference between click event ("event") and "target".
+        var get_distance = function(event, target) {
+            var x_diff = event.offsetX - target.x;
+            var y_diff = event.offsetY - target.y;
+            return Math.sqrt((x_diff * x_diff) + (y_diff * y_diff));
+        };
+
+        // Get the appropriate hint for the distance.
+        var get_distance_hint = function(distance) {
+            if(distance < 26) {
+                return "*clunk* (You're melting!)";
+            } else if(distance < 126) {
+                return "There's something shiny about " + Math.floor(distance) + "m away! (You're burning up!)";
+            } else if(distance < 326) {
+                return "*rattle rattle* (It's getting kinda hot in here...)";
+            } else if(distance < 426) {
+                return "You can see a shovel sticking out of the ground. (It's nice and warm. How cozy.)";
+            } else if(distance < 526) {
+                return "Nothing here... (It's a little chilly...)";
+            } else if(distance < 626) {
+                return "The terrain is very flat. (Your teeth are chattering.)";
+            } else {
+                return "You like sand. How fortunate. (You can't even feel your body, it's so cold.)";
+            }
+        };
+
+        // Initiate the basic variables.
+        var width = 400;
+        var height = 400;
+        var clicks = 0;
+
+        // Initiate the variable "target".
+        var target = {
+            x: get_random_number(width),
+            y: get_random_number(height)
+        };
+
+        $("#remaining").text(25 - clicks + " " + "clicks" + " " + "remaining")
+        
+        $("#map").click(function(event) {
+
+            if(clicks >= 25) {
+                alert("Some pirates swooped through and stole the treasure before you...")
+                location.reload();
+            }
+            clicks++;
+
+
+            $("#remaining").text(25 - clicks + " " + "clicks" + " " + "remaining")
+
+            var distance = get_distance(event, target);
+            var distance_hint = get_distance_hint(distance);
+
+            $("#distance").text(distance_hint)
+            $("#metres").text(Math.floor(distance) + "m" + " " + "away")
+
+            if (distance <= 10) {
+                alert("Found the treasure in " + clicks + " clicks!");
+                location.reload();
+            }
+        });
